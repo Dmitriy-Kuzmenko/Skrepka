6eea93aff87fd88c850bcd5573c843eb863db271
Dmitriy <kuzmenko.dmitriy228@gmail.com>
2021-10-07 14:15:11 +0300

    hello stas

--- main.cpp
+++ main.cpp
@@ -5,7 +5,8 @@ int main(int argc, char *argv[ ])
 {
 QApplication app(argc, argv);
  
-QPushButton button("Hello, World!");
+QPushButton button("Hello, Stas!");
+QPushButton button("Hello, Stas!");
 
 button.resize(200, 700);
 button.resize(200, 720);
