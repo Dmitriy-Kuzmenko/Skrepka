bad368f3b4a6299d80c5ddd7f13b7555ecc49d75
Dmitriy <kuzmenko.dmitriy228@gmail.com>
2021-10-07 14:06:25 +0300

    TEST MERGE BRANCHES

--- main.cpp
+++ main.cpp
@@ -6,7 +6,9 @@ int main(int argc, char *argv[ ])
 QApplication app(argc, argv);
  
 QPushButton button("Hello, World!");
-button.resize(200, 60);
+
+button.resize(200, 700);
+button.resize(200, 720);
 button.show( );
  
 return app.exec( );
